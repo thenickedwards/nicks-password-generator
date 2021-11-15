b43b83f67c3344e2b697d06625810a0babd12fa0
Nick Edwards <theadventuresofnickedwards@gmail.com>
2021-11-15 11:39:43 -0800

    Added function and return password. Required aspects of JS.

--- Develop/script.js
+++ Develop/script.js
@@ -8,10 +8,17 @@ var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChars = ["`", "-", "=", ";", "'", ",", ".", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", ":", "?"];
 
+// Function begins
+function generatePassword() {
+  var password = ""
+  console.log("Generate a password triggered! Starting with a blank password.");
 
 
 
 
+
+  return password
+}
 
 // Write password to the #password input
 function writePassword() {
